81d4da9ea834ac450f12def64ae4da033b83afa5
Sarthak Chhabra <sarthakc2211@gmail.com>
2025-04-02 11:49:49 +0530

    updated for deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: process.cwd(),
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(process.cwd(), 'index.html'),
-    },
-  },
-  server: {
-    port: 3000,
-  },
 })
